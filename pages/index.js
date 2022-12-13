4a3ca1b9f6a2e45c302e13b59d508b11264b8d0e
Diego Prestes <diegoprestes.git@hotmail.com>
2022-12-13 11:10:23 -0300

    feat: home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import styles from '../styles/Home.module.scss'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={styles.fullContent}>
-      <div className={styles.shining}></div>
       <div className={styles.logo}>
         REALLY <br />
         NICE <br />
@@ -14,21 +10,20 @@ export default function Home() {
       </div>
       <div>
         <div className={styles.center}>
-          <div className={inter.className}>
-            <strong className={styles.bigOlText}>
-              DESCOBRINDO FILMES E SÉRIES ATRAVÉS DA RECOMENDAÇÃO
-            </strong>
+          <strong className={styles.bigOlText}>
+            DESCOBRINDO FILMES E SÉRIES ATRAVÉS DA RECOMENDAÇÃO
+          </strong>
 
-            <p className={styles.thinText}>
-              Digite seu e-mail e comece a explorar a coleção.
-            </p>
-            <div className={styles.fullInput}>
-              <input type="email" placeholder="E-MAIL" />
-              <button onClick={() => console.log('GO!')}>ENTRAR</button>
-            </div>
+          <p className={styles.thinText}>
+            Digite seu e-mail e comece a explorar a coleção.
+          </p>
+          <div className={styles.fullInput}>
+            <input type="email" placeholder="E-MAIL" />
+            <button onClick={() => console.log('GO!')}>ENTRAR</button>
           </div>
         </div>
       </div>
+      <div className={styles.shining}></div>
     </div>
   )
 }
