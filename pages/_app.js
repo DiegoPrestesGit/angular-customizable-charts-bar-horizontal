aa33113dc386228f1133af05c54e53799664068b
Diego Prestes <diegoprestes.git@hotmail.com>
2022-12-12 21:14:06 -0300

    config: fonts and start home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import { Changa_One } from '@next/font/google'
+
+const changaOne = Changa_One({ weight: '400', subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={changaOne.className}>
+      <Component {...pageProps} />{' '}
+    </main>
+  )
 }
 
 export default MyApp
