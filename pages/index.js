732f41b3786728ac0a27d6fab4c77eefe0d2bb9c
Diego Prestes <diegoprestes.git@hotmail.com>
2022-12-19 18:43:32 -0300

    style: main page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth'
 import firebaseApp from '../providers/firebase-app'
 import styles from '../styles/Home.module.scss'
+import AuthUserProvider from '../components/firebase/context'
+import { useContext, useState } from 'react'
 
 export default function Home() {
   firebaseApp()
   const googleProvider = new GoogleAuthProvider()
   const authProvider = getAuth()
 
+  const [email, setEmail] = useState('')
+
+  const change = (email) => {
+    if (email == null) email = ''
+    setEmail(email)
+  }
+
   const signIn = async () => {
     const res = await signInWithPopup(authProvider, googleProvider)
+    // const res = await signInWithEmailAndPassword(
+    //   authProvider,
+    //   'diegosousaflo@hotmail.com',
+    //   'Kalisto@123'
+    // )
     console.log(res)
   }
 
@@ -29,13 +49,20 @@ export default function Home() {
             Digite seu e-mail e comece a explorar a coleção.
           </p>
           <div className={styles.fullInput}>
-            <input type="email" placeholder="E-MAIL" />
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => change(event.target.value)}
+              placeholder="E-MAIL"
+            />
             <button onClick={() => signIn()}>ENTRAR</button>
           </div>
-          {/* <div className={styles.loginGoogle}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
-            <p>Entrar com Google</p>
-          </div> */}
+          <div className={styles.loginGoogle}>
+            <button onClick={() => console.log('GO!')}>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+              <p>Entrar com Google</p>
+            </button>
+          </div>
         </div>
       </div>
       <div className={styles.shining}></div>
