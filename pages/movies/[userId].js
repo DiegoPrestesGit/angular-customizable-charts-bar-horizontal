68e47f2228cc3cbb62e8caf0370bb662e69efd90
Diego Prestes <diegoprestes.git@hotmail.com>
2023-01-25 14:25:56 -0300

    feat: search mecanism

--- pages/movies/[userId].js
+++ pages/movies/[userId].js
@@ -1,13 +1,29 @@
 import { withRouter } from 'next/router'
-import { useContext } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { AuthContext } from '../../components/firebase/context'
 import styles from '../../styles/Movies.module.scss'
 import Movie from '../../components/movie'
 import mockMovies from '../../mock-sample.json'
+import { HiMagnifyingGlass } from 'react-icons/hi2'
 
 function Movies({ props: { userRatings }, ...context }) {
   const authContext = useContext(AuthContext)
   const userInfo = authContext.getUserInfo()
+
+  const [isSearch, setIsSearch] = useState(false)
+  const [findedMovies, setFindedMovies] = useState([])
+  const searchInputRef = useRef()
+
+  const searchMovie = () => {
+    const searchValue = searchInputRef.current.value
+    if (searchValue == '') {
+      setIsSearch(false)
+      return
+    }
+    const findIt = mockMovies.filter((mov) => mov.title.includes(searchValue))
+    setIsSearch(true)
+    setFindedMovies(findIt)
+  }
 
   return (
     <div className={styles.fullContent}>
@@ -17,28 +33,58 @@ function Movies({ props: { userRatings }, ...context }) {
           NICE <br />
           LOGO
         </div>
-        <div className="right-side">
-          <strong className={styles.bigOlText}>
-            Bem-vindo, {userInfo && userInfo.user && userInfo.user.displayName}
-          </strong>
-          <button onClick={() => authContext.removeUserAuthInfo()}>SAIR</button>
+        <div className={styles.withSearch}>
+          <div className="right-side">
+            <strong className={styles.bigOlText}>
+              Bem-vindo,{' '}
+              {userInfo && userInfo.user && userInfo.user.displayName}
+            </strong>
+            <button onClick={() => authContext.removeUserAuthInfo()}>
+              SAIR
+            </button>
+          </div>
+          <div className={styles.fullInput}>
+            <input
+              type="text"
+              ref={searchInputRef}
+              placeholder="PROCURAR FILME"
+            />
+            <button onClick={() => searchMovie()}>
+              <HiMagnifyingGlass />
+            </button>
+          </div>
         </div>
       </div>
       <div className={styles.movieList}>
-        {mockMovies.map((singleMovie) => {
-          const rated = userRatings
-            ? userRatings.find((rating) => rating.movieId == singleMovie.id)
-            : undefined
+        {isSearch
+          ? findedMovies.map((singleMovie) => {
+              const rated = userRatings
+                ? userRatings.find((rating) => rating.movieId == singleMovie.id)
+                : undefined
 
-          return (
-            <Movie
-              movieData={singleMovie}
-              key={singleMovie.id}
-              userId={userInfo && userInfo.user.userId}
-              movieRating={(rated && rated.ratingValue) || 0}
-            />
-          )
-        })}
+              return (
+                <Movie
+                  movieData={singleMovie}
+                  key={singleMovie.id}
+                  userId={userInfo && userInfo.user.userId}
+                  movieRating={(rated && rated.ratingValue) || 0}
+                />
+              )
+            })
+          : mockMovies.map((singleMovie) => {
+              const rated = userRatings
+                ? userRatings.find((rating) => rating.movieId == singleMovie.id)
+                : undefined
+
+              return (
+                <Movie
+                  movieData={singleMovie}
+                  key={singleMovie.id}
+                  userId={userInfo && userInfo.user.userId}
+                  movieRating={(rated && rated.ratingValue) || 0}
+                />
+              )
+            })}
       </div>
       <div className={styles.shining}></div>
     </div>
