79c68cd148fe9413d65470001a001e751f7df765
Diego Prestes <diegoprestes.git@hotmail.com>
2023-04-05 12:59:09 -0300

    TS_CRUD (GO_CRUD) env_var

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
+    // START OF FIREBASE ENV_VARS
     FIREBASE_PUBLIC_API_KEY: 'AIzaSyCwPoDB1RW6syYjAgwjjdbxMx_g8yUSd6w',
     FIREBASE_AUTH_DOMAIN: 'themoviebakery.firebaseapp.com',
     FIREBASE_PROJECT_ID: 'themoviebakery',
@@ -10,8 +11,9 @@ const nextConfig = {
     FIREBASE_MESSAGING_SENDER_ID: '456985856502',
     FIREBASE_APP_ID: '1:456985856502:web:c46fb6c765e32f4fbe1fd7',
     FIREBASE_MEASUREMENT_ID: 'G-MFJBJC43KR',
+    // END OF FIREBASE ENV_VARS
     TMDB_POSTER_HOST: 'https://image.tmdb.org/t/p/w500',
-    GO_CRUD: 'http://localhost:8080',
+    GO_CRUD: '172.26.14.61:8080',
   },
 }
 
